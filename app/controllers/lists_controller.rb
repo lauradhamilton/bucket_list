69fa5d9c9e6d4685dad56670e73e2348fb39dc7d
Laura Hamilton <lauradhamilton@gmail.com>
2013-11-09 12:07:47 -0600

    Finished the tutorial at http://railsgirlschi.herokuapp.com/bucket_list_part_1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,42 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      flash[:notice] = "You have successfully added list " + @list.name
+      redirect_to lists_url
+    else
+      render action: "new"
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+
+    if @list.update_attributes(params[:list])
+      redirect_to lists_path
+    else
+      render action: "edit"
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_url
+  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ActiveRecord::Base
   attr_accessible :name, :reason
+  validates :name, presence: true
 end
